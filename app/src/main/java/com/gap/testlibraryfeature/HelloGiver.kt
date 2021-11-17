2884034a45dab8d089869dc294c0f98803780b3a
Diego Recalde <diego.recalde@globant.com>
2021-11-17 16:46:25 -0500

    Test Library

---
+++ app/src/main/java/com/gap/testlibraryfeature/HelloGiver.kt
@@ -0,0 +1,12 @@
+package com.gap.testlibraryfeature
+
+object HelloGiver {
+
+    fun sayHi(): String {
+        return "Hi!"
+    }
+
+    fun sayHello(): String {
+        return "Hello!"
+    }
+}
